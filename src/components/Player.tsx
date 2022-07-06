b5878ffb0b0b4be65d47d0b91026e02d1b15c809
ambertaal <amber.taal@gmail.com>
2022-07-06 12:49:03 +0200

    add sorting

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -2,12 +2,15 @@ import React from "react";
 
 type Props = {
   name: string;
+  score: number;
 };
 
 const Player = (props: Props) => {
   return (
     <li className="Player">
-      <p>{props.name}</p>
+      <p>
+        {props.name} (score: {props.score})
+      </p>
     </li>
   );
 };
--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -2,20 +2,38 @@ import React, { useState } from "react";
 import Player from "./Player";
 import AddPlayerForm from "./AddPlayerForm";
 
+type PlayerObj = {
+  id: number;
+  name: string;
+  score: number;
+};
+
+function compareByScore(playerA: PlayerObj, playerB: PlayerObj) {
+  return playerB.score - playerA.score;
+}
+
 const Scoreboard = () => {
-  const [players, setPlayers] = useState([
+  const [players, setPlayers] = useState<PlayerObj[]>([
     { id: 1, name: "Violeta", score: 11 },
     { id: 2, name: "Eszter", score: 14 },
     { id: 3, name: "Jeroen v2", score: 4 },
     { id: 4, name: "Lisa", score: 42 },
   ]);
 
+  const playersSorted =
+    // first "copy" the array
+    [...players]
+      // then sort it with the `compareByScore` callback function
+      .sort(compareByScore);
+
   return (
     <div className="Scoreboard">
       <h1>Player's scores:</h1>
       <ul>
-        {players.map((player) => {
-          return <Player key={player.id} name={player.name} />;
+        {playersSorted.map((player) => {
+          return (
+            <Player key={player.id} name={player.name} score={player.score} />
+          );
         })}
       </ul>
       <AddPlayerForm />
