1a062d4b2197feb355fe024ecfb8a00503a56815
ambertaal <amber.taal@gmail.com>
2022-07-06 11:16:00 +0200

    change hardcoded data into data into component local state into array

--- src/components/Scoreboard.tsx
+++ src/components/Scoreboard.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Player from "./Player";
 import AddPlayerForm from "./AddPlayerForm";
 
 const Scoreboard = () => {
+  const [players, setPlayers] = useState([
+    { id: 1, name: "Violeta", score: 11 },
+    { id: 2, name: "Eszter", score: 14 },
+    { id: 3, name: "Jeroen v2", score: 4 },
+    { id: 4, name: "Lisa", score: 42 },
+  ]);
+
   return (
     <div className="Scoreboard">
-      <p>Player's scores:</p>
+      <h1>Player's scores:</h1>
       <ul>
-        <Player name="Violeta" />
-        <Player name="Eszter" />
-        <Player name="Jeroen v2" />
-        <Player name="Lisa" />
+        {players.map((player) => {
+          return <Player key={player.id} name={player.name} />;
+        })}
       </ul>
       <AddPlayerForm />
     </div>
